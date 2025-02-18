9bf60681785c62a7b0f4f9227dd405747086071e
christianokuru <okuruchristian@gmail.com>
2025-02-18 03:14:57 +0100

    Added  the other folders

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <h1 class="text-6xl text-red-700 font-bold">hello guys</h1>
+    <h1 class="text-6xl text-green font-bold">green</h1>
+    <p class="text-3xl text-green bg-lemon inline-block">lemon</p>
   </div>
 </template>
