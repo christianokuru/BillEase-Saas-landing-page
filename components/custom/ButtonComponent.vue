d51abe7ee6b04ecfbdadc56c83bddc011a90d989
christianokuru <okuruchristian@gmail.com>
2025-02-18 05:30:25 +0100

    Working on the button component

--- components/custom/ButtonComponent.vue
+++ components/custom/ButtonComponent.vue
@@ -1,7 +1,18 @@
 <script setup>
-
+const props = defineProps({
+    icon: {
+        type: String,
+    },
+    text: {
+        type: String,
+        required: true
+    }
+})
 </script>
 
 <template>
-    
+    <button class="flex items-center text-nowrap py-[13px]">
+        {{ text }}
+        <img :src="icon" alt="icon" class="ml-[76px] mr-[28px]">
+    </button>
 </template>
--- components/custom/NavbarComponent.vue
+++ components/custom/NavbarComponent.vue
@@ -1,6 +1,10 @@
 <script setup>
 import LogoComponent from '@/components/custom/LogoComponent.vue'
+import ButtonComponent from '@/components/custom/ButtonComponent.vue'
+import arrow from '@/assets/icons/arrow-right.svg'
 import { navlinks } from '@/lib/navlinks.js'
+
+
 </script>
 
 <template>
@@ -9,10 +13,18 @@ import { navlinks } from '@/lib/navlinks.js'
             <logo-component />
         </div>
 
-        <div class="flex items-center">
+        <div class="flex items-center mr-[250px]">
             <nav v-for="links in navlinks" :key="links.text">
                 <nuxt-link :to="links.route" class="mr-[36px] text-green text-[16px]">{{ links.text }}</nuxt-link>
             </nav>
+        </div>
+
+        <div class="">
+            <button-component
+            text="Sign In"
+            :icon="arrow"
+            class="bg-green text-lemon "
+            />
         </div>
     </div>
 </template>
