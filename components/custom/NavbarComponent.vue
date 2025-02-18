ce7181e88e4b5b8aa1e1b6c7e61f9a9a55963d8f
christianokuru <okuruchristian@gmail.com>
2025-02-18 11:06:54 +0100

    Added margin to the navbar

--- components/custom/NavbarComponent.vue
+++ components/custom/NavbarComponent.vue
@@ -6,25 +6,29 @@ import { navlinks } from "@/lib/navlinks.js";
 </script>
 
 <template>
-  <div class="flex items-center mt-[40px] mb-[10px]">
-    <div class="mr-[266px]">
-      <logo-component />
-    </div>
+  <div class="mb-[122px]">
+    <div class="flex items-center mt-[40px] mb-[10px]">
+      <div class="mr-[266px]">
+        <logo-component />
+      </div>
 
-    <div class="flex items-center mr-[250px]">
-      <nav v-for="links in navlinks" :key="links.text">
-        <nuxt-link :to="links.route" class="mr-[36px] text-green text-[16px]">{{
-          links.text
-        }}</nuxt-link>
-      </nav>
-    </div>
+      <div class="flex items-center mr-[250px]">
+        <nav v-for="links in navlinks" :key="links.text">
+          <nuxt-link
+            :to="links.route"
+            class="mr-[36px] text-green text-[16px]"
+            >{{ links.text }}</nuxt-link
+          >
+        </nav>
+      </div>
 
-    <div class="">
-      <button-component
-        text="Sign In"
-        :icon="arrow"
-        class="bg-green text-lemon hover:bg-[#0e2e28db]"
-      />
+      <div class="">
+        <button-component
+          text="Sign In"
+          :icon="arrow"
+          class="bg-green text-lemon hover:bg-[#0e2e28db]"
+        />
+      </div>
     </div>
   </div>
 </template>
