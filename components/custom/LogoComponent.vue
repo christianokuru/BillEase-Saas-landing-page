59543bc835f448456af1a4a4e1bfade84e1443e4
christianokuru <okuruchristian@gmail.com>
2025-02-18 04:09:12 +0100

    Creating the navbar

--- components/custom/LogoComponent.vue
+++ components/custom/LogoComponent.vue
@@ -1,7 +1,9 @@
 <script setup>
-
+import logo from '@/assets/icons/Logo.svg'
 </script>
 
 <template>
-    
+    <div class="w-[142px]">
+        <img :src="logo" alt="bill-ease-logo">
+    </div>
 </template>
--- components/custom/NavbarComponent.vue
+++ components/custom/NavbarComponent.vue
@@ -1,7 +1,18 @@
 <script setup>
-
+import LogoComponent from '@/components/custom/LogoComponent.vue'
+import { navlinks } from '@/lib/navlinks.js'
 </script>
 
 <template>
-    
+    <div class="border mt-[40px] mb-[10px]">
+        <div>
+            <logo-component />
+        </div>
+
+        <div>
+            <nav v-for="links in navlinks" :key="links.text">
+                <nuxt-link :to="links.route">{{ links.text }}</nuxt-link>
+            </nav>
+        </div>
+    </div>
 </template>
