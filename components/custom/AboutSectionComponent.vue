49efc14f2125ffba924289456d66c4f19d342baf
christianokuru <okuruchristian@gmail.com>
2025-02-20 06:16:27 +0100

    Started the about section

---
+++ components/custom/AboutSectionComponent.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="border">
+        <div class="border border-red-500 w-[1000px] mx-auto">
+           <h1>snasdjcnldjvjsdv</h1> 
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeaderSectionComponent from '@/components/custom/HeaderSectionComponent.vue';
+import AboutSectionComponent from '~/components/custom/AboutSectionComponent.vue';
 </script>
 
 <template>
     <div>
         <header-section-component />
+        <about-section-component />
     </div>
 </template>
