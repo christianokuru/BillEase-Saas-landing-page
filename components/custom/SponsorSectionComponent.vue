6be4fce57e9acbacaf9aa70525c6e8eaa983e7ab
christianokuru <okuruchristian@gmail.com>
2025-02-21 00:00:32 +0100

    finished the sponsor section for desktop

---
+++ components/custom/SponsorSectionComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+import first from "@/assets/images/1.svg"
+import second from "@/assets/images/2.svg"
+import third from "@/assets/images/3.svg"
+import fourth from "@/assets/images/4.svg"
+import fifth from "@/assets/images/5.svg"
+import sixth from "@/assets/images/6.svg"
+
+const images = [first, second, third, fourth, fifth, sixth]
+</script>
+
+<template>
+    <div class="border bg-green mx-[-120px] flex items-center space-x-[79.6px] px-[221px] py-[47.57px] mb-[160px]">
+        <div v-for="(image, index) in images" :key="index">
+            <div>
+                <img :src="image" alt="sponsor-logo" class="space-x-[79.6px] animate-pulse">
+            </div>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HeaderSectionComponent from '@/components/custom/HeaderSectionComponent.vue';
-import AboutSectionComponent from '~/components/custom/AboutSectionComponent.vue';
+import AboutSectionComponent from '@/components/custom/AboutSectionComponent.vue';
+import SponsorSectionComponent from '@/components/custom/SponsorSectionComponent.vue';
 </script>
 
 <template>
     <div>
         <header-section-component />
         <about-section-component />
+        <sponsor-section-component />
     </div>
 </template>
