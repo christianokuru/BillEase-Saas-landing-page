b09e3c42aafca30d063410dcdb03730b040ccb76
christianokuru <okuruchristian@gmail.com>
2025-02-20 19:22:03 +0100

    finshed the mobile about section

--- components/custom/AboutSectionComponent.vue
+++ components/custom/AboutSectionComponent.vue
@@ -4,10 +4,12 @@ import ButtonComponent2 from "@/components/custom/ButtonComponent2.vue";
 
 <template>
   <div class="mb-[160px] max-sm:w-[349px]">
-    <div class="flex items-center justify-between w-[1008px] ml-[95px] max-sm:w-[327px] max-sm:ml-[0px] max-sm:border">
-      <div>
+    <div
+      class="flex items-center justify-between w-[1008px] ml-[95px] max-sm:w-[327px] max-sm:ml-[0px] max-sm:border max-sm:flex-col"
+    >
+      <div class="border border-blue-600 max-sm:w-[327px] max-sm:mb-[88px]">
         <!-- text div -->
-        <div class="w-[493px] border border-red-500">
+        <div class="w-[493px] border border-red-500 max-sm:w-[327px]">
           <div class="max-sm:w-[327px] max-sm:mb-[24px] border">
             <h1
               class="text-green text-heading2 w-[475px] leading-[56px] mb-[24px] max-sm:text-sub-heading max-sm:w-[327px] max-sm:text-left max-sm:leading-[48px]"
@@ -16,13 +18,17 @@ import ButtonComponent2 from "@/components/custom/ButtonComponent2.vue";
             </h1>
           </div>
           <div>
-            <p class="text-green w-[445px] text-body-lg leading-[28px] max-sm:w-[327px] max-sm:text-body-md">
+            <p
+              class="text-green w-[445px] text-body-lg leading-[28px] max-sm:w-[327px] max-sm:text-body-md"
+            >
               Thrive with efficient invoicing, ensuring you stay on growth, not
               paperwork.
             </p>
           </div>
 
-          <div class="flex items-center mt-[49px] w-[439px] max-sm:flex-col max-sm:w-[327px] max-sm:items-start border">
+          <div
+            class="flex items-center mt-[49px] w-[439px] max-sm:flex-col max-sm:w-[327px] max-sm:items-start border"
+          >
             <button-component2
               text="Unlimited Capacity"
               class="bg-lemon text-green w-[223px] font-[600] mr-[24px] hover:bg-[#d1eb089d] max-sm:mb-[16px]"
@@ -35,10 +41,16 @@ import ButtonComponent2 from "@/components/custom/ButtonComponent2.vue";
         </div>
       </div>
       <!-- right div -->
-      <div class="w-[324px]">
-        <p class="border-b border-green pb-[40.45px] text-green text-body-lg">Create and send invoices in seconds</p>
-        <p class="border-b border-green py-[40.45px] text-green text-body-lg">Pay bills on time without missing a beat</p>
-        <p class="pt-[40.45px] text-green text-body-lg">Control your cash flow on-demand</p>
+      <div class="w-[324px] border border-green-600">
+        <p class="border-b border-green pb-[40.45px] text-green text-body-lg">
+          Create and send invoices in seconds
+        </p>
+        <p class="border-b border-green py-[40.45px] text-green text-body-lg">
+          Pay bills on time without missing a beat
+        </p>
+        <p class="pt-[40.45px] text-green text-body-lg">
+          Control your cash flow on-demand
+        </p>
       </div>
     </div>
   </div>
