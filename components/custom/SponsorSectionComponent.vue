d1b322f099a85cc5e0efd922a1e6c33cfb201f11
christianokuru <okuruchristian@gmail.com>
2025-02-21 02:49:35 +0100

    Added the bg color and ml to the desctop component

--- components/custom/SponsorSectionComponent.vue
+++ components/custom/SponsorSectionComponent.vue
@@ -33,7 +33,7 @@ const displayedImages = computed(() => {
 </script>
 
 <template>
-  <div class="bg-green w-[1440px] flex items-center space-x-[79.6px] px-[221px] py-[47.57px] mb-[160px] max-sm:mb-[64px] max-sm:w-[375px] max-sm:px-1 max-sm:ml-[-24px]">
+  <div class="bg-green ml-[-120px] w-[1440px] flex items-center space-x-[79.6px] px-[221px] py-[47.57px] mb-[160px] max-sm:mb-[64px] max-sm:w-[375px] max-sm:px-1 max-sm:ml-[-24px]">
     <div v-for="(image, index) in displayedImages" :key="index" class="max-sm:mx-[12px]">
       <img :src="image" alt="sponsor-logo" class="space-x-[79.6px]">
     </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@ import FooterComponent from "@/components/custom/FooterComponent.vue";
 </script>
 
 <template>
-  <div class="">
+  <div class="bg-background">
     <main class="border max-sm:w-[375px] max-sm:px-[24px] px-[118px] w-[1440px] mx-auto">
     <navbar-component />
     <nuxt-page />
