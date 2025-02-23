d314d821335500c1881890cd267200b531ddb96b
christianokuru <okuruchristian@gmail.com>
2025-02-23 13:52:03 +0100

    Added the bg color to the page

--- layouts/AuthLayout.vue
+++ layouts/AuthLayout.vue
@@ -1,13 +1,14 @@
 <script setup>
 import NavbarComponent from "@/components/custom/NavbarComponent.vue";
 import FooterComponent from "@/components/custom/FooterComponent.vue";
-
 </script>
 
 <template>
-<div class="w-[1440px] px-[118px] mb-[112px] mx-auto h-screen">
-    <navbar-component />
-    <nuxt-page />
-    <footer-component />
-</div>
+  <div class="bg-background">
+    <div class="w-[1440px] px-[118px] mb-[112px] mx-auto">
+      <navbar-component />
+      <nuxt-page />
+      <footer-component />
+    </div>
+  </div>
 </template>
